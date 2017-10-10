aad8d855cdd1e71d92faac9837520d57cbd397e3
Yogen Rai <yogen.rai.992@gmail.com>
2017-10-10 13:27:40 -0500

    Fixed the configuration loading issue for particular module.

--- kraker-info-configuration/src/main/java/com/eprogrammerz/examples/configuration/KrakerInfoConfigurationApplication.java
+++ kraker-info-configuration/src/main/java/com/eprogrammerz/examples/configuration/KrakerInfoConfigurationApplication.java
@@ -3,9 +3,11 @@ package com.eprogrammerz.examples.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class KrakerInfoConfigurationApplication {
 
 	public static void main(String[] args) {
--- kraker-info-user-svc/src/main/java/com/eprogrammerz/examples/userSvc/controllers/UserController.java
+++ kraker-info-user-svc/src/main/java/com/eprogrammerz/examples/userSvc/controllers/UserController.java
@@ -4,7 +4,9 @@ import com.eprogrammerz.examples.userSvc.clients.BookmarkClient;
 import com.eprogrammerz.examples.userSvc.models.Bookmark;
 import com.eprogrammerz.examples.userSvc.models.User;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,13 +18,19 @@ import java.util.List;
 /**
  * Created by Yogen on 9/26/2017.
  */
+@Slf4j
 @RestController
 @RequiredArgsConstructor(onConstructor = @_(@Autowired))
 public class UserController {
+    @Value("${env.current:local}")
+    private String currentEnv;
+
     private final BookmarkClient bookmarkClient;
 
     @GetMapping("/users/{userId}")
     public ResponseEntity<User> getUserInfo(@PathVariable("userId") String userId) {
+        log.info("Current Environment: {}", currentEnv);
+
         List<Bookmark> bookmarks = this.bookmarkClient.getBookmarks(userId);
         User user = new User(userId, bookmarks);
         return new ResponseEntity<User>(user, HttpStatus.OK);
