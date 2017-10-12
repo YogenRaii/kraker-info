a8c786277fe6b7d4b29b51e70612b2671edecc82
Yogen Rai <yogen.rai.992@gmail.com>
2017-10-12 16:52:06 -0500

    Disabled security for easiness in development.

--- kraker-info-gateway/src/main/java/com/eprogrammerz/examples/gateway/ZuulProxyServerApplication.java
+++ kraker-info-gateway/src/main/java/com/eprogrammerz/examples/gateway/ZuulProxyServerApplication.java
@@ -2,14 +2,13 @@ package com.eprogrammerz.examples.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableSwagger2
-@EnableOAuth2Sso
+//@EnableOAuth2Sso
 public class ZuulProxyServerApplication {
 
     public static void main(String[] args) {
